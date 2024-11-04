0ee3af656cd463f5dc4028c74e5f931e56153ab9
callumbutteryfd <callum.buttery@fanduel.com>
2024-11-04 16:41:39 +0000

    Add title to list of blogs

--- src/Pages/Blog/BlogList/BlogList.tsx
+++ src/Pages/Blog/BlogList/BlogList.tsx
@@ -16,20 +16,25 @@ export const BlogList: React.FC = () => {
 	}, []);
 
 	return (
-		<div className="flex justify-center mt-48">
+		<div className="flex justify-center mt-32">
 			{blogs.length ? (
-				<div className="card lg:card-side bg-base-100 shadow-2xl">
-					<figure>
-						<img
-							src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-							alt="Album"
-						/>
-					</figure>
-					<div className="card-body">
-						<h2 className="card-title">New album is released!</h2>
-						<p>Click the button to listen on Spotiwhy app.</p>
-						<div className="card-actions justify-end">
-							<button className="btn btn-primary">Listen</button>
+				<div>
+					<div className="text-4xl flex justify-center pb-24 tracking-widest">
+						List of Blogs
+					</div>
+					<div className="card lg:card-side bg-base-100 shadow-2xl">
+						<figure>
+							<img
+								src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+								alt="Album"
+							/>
+						</figure>
+						<div className="card-body">
+							<h2 className="card-title">New album is released!</h2>
+							<p>Click the button to listen on Spotiwhy app.</p>
+							<div className="card-actions justify-end">
+								<button className="btn btn-primary">Listen</button>
+							</div>
 						</div>
 					</div>
 				</div>
