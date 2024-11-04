b0de6133ac5433ebd1ca2bfd5947f0385391a7e0
callumbutteryfd <callum.buttery@fanduel.com>
2024-11-04 14:13:08 +0000

    Style changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { Navbar } from "./Nav/Navbar";
 function App() {
 	return (
 		<>
-			<Navbar />
-			<div className="flex justify-center text-2xl">Hello there</div>
+			<div className="w-3/4 mx-auto my-5">
+				<Navbar />
+				<div className="flex justify-center text-2xl">Hello there</div>
+			</div>
 		</>
 	);
 }
