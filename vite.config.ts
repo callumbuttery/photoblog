d0c003444ff7e9ff5eb7dfa6a40e93af175d4f29
callumbutteryfd <callum.buttery@fanduel.com>
2024-11-04 11:57:44 +0000

    Dotenv set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { config } from "dotenv";
+import react from "@vitejs/plugin-react";
+
+// Load environment variables
+config();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	define: {
+		"process.env": process.env,
+	},
+});
