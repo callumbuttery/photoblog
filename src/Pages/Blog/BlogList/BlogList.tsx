3a6ab34c0be68de6537a6391ba51457bcce32c1b
callumbutteryfd <callum.buttery@fanduel.com>
2024-11-05 11:15:09 +0000

    Formatting for blog

--- src/Pages/Blog/BlogList/BlogList.tsx
+++ src/Pages/Blog/BlogList/BlogList.tsx
@@ -10,45 +10,43 @@ export const BlogList: React.FC = () => {
 		const fetchEntries = async () => {
 			const entries = await getAllEntries();
 			setBlogs(entries);
-			console.log({ blogs });
 		};
 
 		fetchEntries();
 	}, []);
 
 	return (
-		<div className="flex justify-center mt-32">
+		<div className="mt-24 max-w-fit flex flex-col mx-auto">
+			<div>
+				<h1 className="text-4xl font-bold">Latest</h1>
+				<h2 className="text-xl mt-3">A blog about my adventures</h2>
+				<div className="divider"></div>
+			</div>
 			{blogs.length ? (
-				blogs.map(
-					(blog: Blog) => (
-						console.log(
-							"blog.fields.blogImage.fields.url",
-							blog.fields.blogImage
-						),
-						(
-							<div key={blog.sys.id}>
-								<div className="card lg:card-side bg-base-100 shadow-2xl h-2/4 w-2/4 mx-auto">
-									<figure>
-										<img
-											src={blog.fields.blogImage.fields.file.url}
-											alt="Album"
-											className="object-cover"
-										/>
-									</figure>
-									<div className="card-body">
-										<h2 className="card-title">{blog.fields.blogTitle}</h2>
-										<p>{blog.fields.postContent}</p>
-										<p>{blog.fields.blogDate}</p>
-										<p>{blog.fields.blogAuthor}</p>
-										<div className="card-actions justify-end">
-											<button className="btn btn-primary">View</button>
-										</div>
+				blogs.map((blog: Blog) => (
+					<div key={blog.sys.id} className="flex justify-center mt-16">
+						<div className="card lg:card-side bg-base-100 shadow-2xl h-fit mx-auto w-full">
+							<figure className="max-w-lg max-h-96">
+								<img
+									src={blog.fields.blogImage.fields.file.url}
+									alt="Album"
+									className="object-contain"
+								/>
+							</figure>
+							<div className="w-96 p-4 flex flex-col">
+								<h3 className="card-title text-2xl">{blog.fields.blogTitle}</h3>
+								<p className="mt-2 text-lg">{blog.fields.blogSummary}</p>
+								<div className="mt-auto mb-2">
+									<p>{blog.fields.blogDate}</p>
+									<p>{blog.fields.blogAuthor}</p>
+									<div className="card-actions justify-center mt-3">
+										<button className="btn btn-primary">View</button>
 									</div>
 								</div>
 							</div>
-						)
-					)
-				)
+						</div>
+					</div>
+				))
 			) : (
 				<BlogLoadingSkeleton />
 			)}
