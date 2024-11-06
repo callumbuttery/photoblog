b1ecaec1086d3e0333fec240d862311a31a5338f
callumbutteryfd <callum.buttery@fanduel.com>
2024-11-06 11:40:42 +0000

    font awesome icons + styling changes to about page

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,24 +1,37 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import me from "../../assets/me.jpg";
+
 export const About: React.FC = () => {
 	return (
-		<div className="flex flex-col items-center gap-8">
-			<h1 className="font-bold text-3xl self-center">Meet the crew</h1>
-			<div className="grid grid-cols-1 sm:grid-cols-3 gap-20">
-				<div className="flex gap-4">
-					<img alt="Logo" src="/avatar.png" className="rounded-full w-24" />
-					<div className="flex flex-col gap-2">
-						<h3 className="font-bold">David Forren</h3>
-						<span className="text-sm">Founder / CEO</span>
-						<div className="flex text-accent text-xs">
-							<a className="btn btn-ghost btn-sm btn-circle">
-								<i className="fa-brands fa-github text-lg"></i>
-							</a>
-							<a className="btn btn-ghost btn-sm btn-circle">
-								<i className="fa-brands fa-twitter text-lg"></i>
-							</a>
-							<a className="btn btn-ghost btn-sm btn-circle">
-								<i className="fa-brands fa-facebook text-lg"></i>
-							</a>
-						</div>
+		<div className="flex flex-col h-96 mt-24">
+			<div className="flex m-auto gap-4">
+				<img alt="Logo" src={me} className="rounded-full w-24 h-28" />
+				<div className="flex flex-col gap-2">
+					<h3 className="font-bold">Callum Buttery</h3>
+					<span className="text-sm">Photographer / Software Engineer</span>
+					<div className="flex text-accent text-xs">
+						<a
+							className="btn btn-ghost btn-xl btn-circle"
+							onClick={() =>
+								window.open("https://github.com/callumbuttery", "_blank")
+							}
+							rel="noopener noreferrer"
+						>
+							<FontAwesomeIcon icon={faGithub} size="2xl" />
+						</a>
+						<a
+							className="btn btn-ghost btn-xl btn-circle"
+							onClick={() =>
+								window.open(
+									"https://www.linkedin.com/in/callumbuttery/",
+									"_blank"
+								)
+							}
+							rel="noopener noreferrer"
+						>
+							<FontAwesomeIcon icon={faLinkedin} size="2xl" />
+						</a>
 					</div>
 				</div>
 			</div>
